e87f7a8f0726d737c515d6c9815570593c48ceb0
lozaning <lozaning@gmail.com>
2020-01-20 05:46:50 -0600

    Update atmega32u4_jiggler_v0.0.0.1.ino

--- atmega32u4_jiggler_v0.0.0.1/atmega32u4_jiggler_v0.0.0.1.ino
+++ atmega32u4_jiggler_v0.0.0.1/atmega32u4_jiggler_v0.0.0.1.ino
@@ -21,7 +21,7 @@
 //Pins defined for the sonar sensor
 int trigPin = 21;    // Trigger
 int echoPin = 20;    // Echo
-long duration, cm, inches;
+long duration, inches;
 
 //Create display object with defined paramaters
 TM1637Display display(CLK, DIO);
@@ -36,42 +36,18 @@ void setup() {
 }
  
 void loop() {
-  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(5);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+ 
 
   int k;
   uint8_t data[] = { 0xff, 0xff, 0xff, 0xff };
   uint8_t blank[] = { 0x00, 0x00, 0x00, 0x00 };
   display.setBrightness(0x0f);
  
-  // Read the signal from the sensor: a HIGH pulse whose
-  // duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
-  pinMode(echoPin, INPUT);
-  duration = pulseIn(echoPin, HIGH);
  
-  // Convert the time into a distance
-  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
-  inches = (duration/2) / 74;   // Divide by 74 or multiply by 0.0135
-  
-  Serial.print(inches);
-  Serial.print("in, ");
-  Serial.print(cm);
-  Serial.print("cm");
-  Serial.println();
-  display.showNumberDec(inches, false);
-  Serial.println(duration);
-  Serial.println();
-  
-  delay(25);
 }
 
-void getInches() {
+int getInches() {
+  long duration, inches;
    // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
   // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
   digitalWrite(trigPin, LOW);
@@ -90,18 +66,18 @@ void getInches() {
   inches = (duration/2) / 74;   // Divide by 74 or multiply by 0.0135
   Serial.print("in: ");
   Serial.println(inches);
-  return inches
+  return inches;
 }
 
-void MoveMouse(squares) {
-  serial.print("Moving the Mouse ");
-  serial.print(squares);
-  serial.println(" times");
-  for (;squares >=0; squares--) {
-  Mouse.move(20, 0, 0);
-  Mouse.move(0, -20, 0);
-  Mouse.move(-20, 0, 0);
-  Mouse.move(0, -20, 0);
-  }
-
+void MoveMouse(int loops) {
+  Serial.print("Moving the Mouse ");
+  Serial.print(loops);
+  Serial.println(" times");
+  for (;loops >=0; loops--) {
+    Mouse.move(20, 0, 0);
+    Mouse.move(0, -20, 0);
+    Mouse.move(-20, 0, 0);
+    Mouse.move(0, -20, 0);
+    }
+  Serial.println("Done moving the mouse");
 }
