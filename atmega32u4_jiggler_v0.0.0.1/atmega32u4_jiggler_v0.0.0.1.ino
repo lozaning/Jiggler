25417bdf8b5491db99fdf1959f73378caad77300
lozaning <lozaning@gmail.com>
2020-01-20 04:18:26 -0600

    Create atmega32u4_jiggler_v0.0.0.1.ino

---
+++ atmega32u4_jiggler_v0.0.0.1/atmega32u4_jiggler_v0.0.0.1.ino
@@ -0,0 +1,75 @@
+/*
+ * created by Rui Santos, https://randomnerdtutorials.com
+ * 
+ * Complete Guide for Ultrasonic Sensor HC-SR04
+ *
+    Ultrasonic sensor Pins:
+        VCC: +5VDC
+        Trig : Trigger (INPUT) - Pin11
+        Echo: Echo (OUTPUT) - Pin 12
+        GND: GND
+ */
+#include <Arduino.h>
+#include <TM1637Display.h>
+#include "Mouse.h"
+
+// Pins defined for 7 segment display library
+#define CLK 18
+#define DIO 19
+
+
+//Pins defined for the sonar sensor
+int trigPin = 21;    // Trigger
+int echoPin = 20;    // Echo
+long duration, cm, inches;
+
+//Create display object with defined paramaters
+TM1637Display display(CLK, DIO);
+ 
+void setup() {
+  //Serial Port begin
+  Serial.begin (9600);
+  //Define inputs and outputs
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+ 
+void loop() {
+  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  int k;
+  uint8_t data[] = { 0xff, 0xff, 0xff, 0xff };
+  uint8_t blank[] = { 0x00, 0x00, 0x00, 0x00 };
+  display.setBrightness(0x0f);
+ 
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+ 
+  // Convert the time into a distance
+  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+  inches = (duration/2) / 74;   // Divide by 74 or multiply by 0.0135
+  
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  display.showNumberDec(inches, false);
+  Serial.println(duration);
+  Serial.println();
+  
+  delay(25);
+}
+
+void MoveMouse() {
+  
+}
